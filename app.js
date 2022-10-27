9dccd76953677204da1102776542bddd27687b0a
Ana <zlatkovic.ana@gmail.com>
2022-10-27 11:22:59 +0200

    mobile menu open styles

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const burger = document.getElementById("burger-btn");
 const menu = document.getElementById("nav-items-wrapper");
+const navWrapper = document.getElementById("navigation-wrapper");
 const hasSubmenu = document.getElementsByClassName("has-dropdown");
+const burgerBar = document.querySelector("#burger-btn span");
+
+// console.log(burgerBar);
 
 burger.addEventListener("click", (e) => {
+    navWrapper.classList.toggle('mobile-menu-open')
     menu.classList.toggle('hidden');
+    burgerBar.classList.toggle('x');
 });
 
 Array.from(hasSubmenu).forEach((submenuWrapper) => {
