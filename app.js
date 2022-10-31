226cb2952fe2fd49fcce554dde319c809317723f
Ana <zlatkovic.ana@gmail.com>
2022-10-31 15:45:59 +0100

    menu fixes

--- app.js
+++ app.js
@@ -5,52 +5,63 @@ const nav = document.getElementById("nav");
 const hasSubmenu = document.getElementsByClassName("has-dropdown");
 const burgerBar = document.querySelector("#burger-btn span");
 
-// // console.log(burgerBar);
-
-// burger.addEventListener("click", (e) => {
-//     navWrapper.classList.toggle('mobile-menu-open')
-//     menu.classList.toggle('hidden');
-//     burgerBar.classList.toggle('x');
-// });
-
-// Array.from(hasSubmenu).forEach((submenuWrapper) => {
-//     console.log(submenuWrapper);
-//     submenuWrapper.addEventListener("click", (e) => {
-
-//     e.target.children[0].classList.toggle("hidden");
-
-
-//     });
-//   });
-
+let mobileMenuOpen=false;
 
 burger.addEventListener("click", (e) => {
-    nav.classList.toggle('o')
-    // menu.classList.toggle('hidden');
-    burgerBar.classList.toggle('x');
+  nav.classList.toggle("o");
+  if (nav.classList.contains("o")) {
+    mobileMenuOpen = true;
+    console.log(mobileMenuOpen);
+  } else {
+    mobileMenuOpen = false;
+    console.log(mobileMenuOpen);
+  }
+  burgerBar.classList.toggle("x");
 });
 
 Array.from(hasSubmenu).forEach((submenuWrapper) => {
-    console.log(submenuWrapper);
-    submenuWrapper.addEventListener("click", (e) => {
-
-    e.target.children[0].classList.toggle("hidden");
-    // e.stopPropagation();
+  if (hasSubmenu || hasSubmenu != null) {
+    Array.from(hasSubmenu).forEach((submenuWrapper) => {
+      submenuWrapper.children[0].classList.add("hidden");
     });
+  }
+  submenuWrapper.addEventListener("click", (e) => {
+    // e.stopPropagation();
+    // if (hasSubmenu || hasSubmenu != null) {
+    //   Array.from(hasSubmenu).forEach((submenuWrapper) => {
+    //     submenuWrapper.children[0].classList.add("hidden");
+    //   });
+    // }
+    // if (e.target.classList.contains("has-dropdown")) {
+    //     e.target.children[0].classList.toggle("hidden");
+    // }
+    if (e.target.children[0].classList.contains("hidden")) { 
+      console.log(e.target.children[0]);
+      e.target.children[0].classList.remove("hidden");
+    } else {
+      console.log(e.target.children[0]);
+      e.target.children[0].classList.add("hidden");
+    }
   });
+});
 
 // Close submenus if user clicks outside of submenu
-  document.addEventListener("click", (e) => {
-    console.log(e);
-    let targetElWithDropdown = e.target.classList.contains("has-dropdown");
-    if(!targetElWithDropdown) {
-        console.log("nema meni");
-        Array.from(hasSubmenu).forEach((submenuWrapper) => {
-            if (!submenuWrapper.children[0].classList.contains("hidden")) {
-                submenuWrapper.children[0].classList.add("hidden");
-            }            
-        });
-    } else {
-        console.log('podmeni!');        
-    }
-});
+
+if (!mobileMenuOpen) {
+    
+    document.addEventListener("click", (e) => {    
+      e.stopPropagation();
+        let targetElWithDropdown = e.target.classList.contains("has-dropdown");
+        if (!targetElWithDropdown) {
+            Array.from(hasSubmenu).forEach((submenuWrapper) => {
+                if (!submenuWrapper.children[0].classList.contains("hidden")) {
+                    submenuWrapper.children[0].classList.add("hidden");
+                }
+            });
+        } else {
+            console.log("clicked outside of submenu!");
+        }          
+    });
+} else {
+    console.log("ddfdfb!");
+}
